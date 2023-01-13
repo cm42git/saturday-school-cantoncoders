d6e68f376df378089b4542644a6fbdadd8530a12
cm42git <106208621+cm42git@users.noreply.github.com>
2023-01-13 15:08:40 -0500

    fixed stack works before refactor

--- stacks-queues/java/src/main/java/org/example/stackqueues/FixedStack.java
+++ stacks-queues/java/src/main/java/org/example/stackqueues/FixedStack.java
@@ -5,6 +5,7 @@ import java.util.EmptyStackException;
 public class FixedStack {
 
     private Object[] data;
+    private int lastInIndex;
 
     public FixedStack(int capacity) {
         this.data = new Object[capacity];
@@ -15,25 +16,66 @@ public class FixedStack {
     }
 
     public void push(Object object) {
+        int i = 0;
+        for (; i < data.length; i++) {
+            if (data[i] == null)
+                break;
+        }
+        if (i != data.length)
+            data[i] = object;
+        else
+            throw new StackFullException();
     }
 
     public Object pop() {
-        return null;
+        int i = 0;
+        for (; i < data.length; i++) {
+            if (data[i] == null)
+                break;
+        }
+        i--;
+        if (i >= 0) {
+            Object out = data[i];
+            data[i] = null;
+            return out;
+        } else
+            throw new EmptyStackException();
     }
 
     public Object peek() {
-        return null;
+        int i = 0;
+        for (; i < data.length; i++) {
+            if (data[i] == null)
+                break;
+        }
+        i--;
+        if (i >= 0)
+            return data[i];
+        else
+            throw new EmptyStackException();
     }
 
     public boolean isFull() {
-        return false;
+        for (Object object : data) {
+            if (object == null)
+                return false;
+        }
+        return true;
     }
 
     public boolean isEmpty() {
-        return false;
+        for (Object object : data) {
+            if (object != null)
+                return false;
+        }
+        return true;
     }
 
     public int search(Object object) {
+        for (int i = 0; i < data.length; i++) {
+            if (object.equals(data[i]))
+                return data.length - i;
+        }
         return -1;
     }
 }
